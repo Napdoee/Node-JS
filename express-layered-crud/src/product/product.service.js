3c76371a6248714f0a5511576b106db1a92a5409
Napdoee <hidayat0249@gmail.com>
2023-11-04 11:49:08 +0800

    feat: initial commit

---
+++ express-layered-crud/src/product/product.service.js
@@ -0,0 +1,59 @@
+//Handle business logic
+const { 
+	findProducts, 
+	findProductById, 
+	findProductByName,
+	insertProduct,
+	deleteProduct,
+	editProduct
+} = require('./product.repository');
+
+const getAllProducts = async () => {
+	const products = await findProducts();
+
+	return products;
+}
+
+const getProductById = async (id) => {
+	const product = await findProductById(id);
+
+	if(!product) {
+		throw Error("Product not found");
+	}
+
+	return product;
+}
+
+const createProduct =  async (newProductData) => {
+	const findProduct = await findProductByName(newProductData.name);
+
+	if(findProduct) {
+		throw Error("name has to be unique")
+	}
+
+	const product = await insertProduct(newProductData)
+
+	return product
+}
+
+const deleteProductById = async (id) => {
+	await getProductById(id);
+
+	await deleteProduct(id);
+}
+
+const editProductById = async (id, productData) => {
+	await getProductById(id);
+
+	const product = await editProduct(id, productData);
+
+	return product;
+}
+
+module.exports = {
+	getAllProducts,
+	getProductById,
+	createProduct,
+	deleteProductById,
+	editProductById
+}
